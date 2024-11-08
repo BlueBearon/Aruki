7b1a3e08ecd3882a6465ef13518203c30f3d5b58
BlueBearon <packerchase@hotmail.com>
2024-11-08 14:38:56 -0500

    init Java Springboot backend

---
+++ Backend/aruki/src/main/java/com/aruki/aruki/ArukiApplication.java
@@ -0,0 +1,13 @@
+package com.aruki.aruki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArukiApplication.class, args);
+	}
+
+}
---
+++ Backend/aruki/src/test/java/com/aruki/aruki/ArukiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aruki.aruki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArukiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
